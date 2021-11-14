8fe017e3bd3dd01b9f112b9886c0d477264fe0f0
Luis J. Romero <romeroluisj@gmail.com>
2021-11-14 10:20:02 -0500

    Window class inherits tk.Tk class

--- source/tk/window.py
+++ source/tk/window.py
@@ -1,6 +1,23 @@
 import tkinter as tk
 
 
+# https://www.pythontutorial.net/tkinter/tkinter-object-oriented-frame/
+class Window(tk.Tk):
+
+    def __init__(self, title="Window Title", geometry="500x500"):
+        super().__init__()
+        self.title(title)
+        self.geometry(geometry)
+        self.lift()
+        self.attributes("-topmost", True)
+        self.after_idle(self.attributes, "-topmost", False)
+
+    def start(self):
+        self.mainloop()
+
+
+"""
+# without inheritance from tk.Tk
 class Window:
 
     def __init__(self, title="Window Title", geometry="500x500"):
@@ -13,3 +30,4 @@ class Window:
 
     def start(self):
         self.window.mainloop()
+"""
