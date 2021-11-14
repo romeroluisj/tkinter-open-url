c700fcc2c83719b1f0d59241362b9f694416eb83
Luis J. Romero <romeroluisj@gmail.com>
2021-11-13 19:37:14 -0500

    Updated git bash script

---
+++ git_sync_dev_main_after_commit.sh
@@ -0,0 +1,15 @@
+# dev branch: push local to remote
+git checkout dev
+git status
+git pull origin dev
+git push origin dev
+
+# merge dev branch into main branch
+git checkout main
+git status
+git merge dev
+
+# main branch: push local to remote
+git status
+git pull origin main
+git push origin main
--- git_update.sh
+++
@@ -1,5 +0,0 @@
-git branch
-git checkout dev
-git status
-git pull origin dev
-git push origin dev
