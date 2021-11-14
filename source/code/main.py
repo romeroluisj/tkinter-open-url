92758a1ea7bed6c0b716e910b9db517a97d08ec2
Luis J. Romero <romeroluisj@gmail.com>
2021-11-14 04:44:23 -0500

    Renamed main_window, cleaned up main.py

--- source/code/main.py
+++ source/code/main.py
@@ -1,9 +1,7 @@
-# Press ⌃R to execute this Python script.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from source.tk.window import Window
 
 # ref:
 # https://www.freecodecamp.org/news/if-name-main-python-example/
 if __name__ == '__main__':
-    window = Window()
-    window.start()
+    main_window = Window()
+    main_window.start()
