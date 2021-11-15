8ebabdddfa1c3b01e1f585b4ebd7e9784db9b83a
Luis J. Romero <romeroluisj@gmail.com>
2021-11-15 07:08:47 -0500

    Added borderwidth (bd) and relief to Frame class

--- source/tk/frame.py
+++ source/tk/frame.py
@@ -5,6 +5,7 @@ class Frame(tk.Frame):
 
     def __init__(self, window):
         super().__init__(window)
+        self.config(bd="2", relief=tk.GROOVE)
         self.grid(row=0, column=0)
         tk.Label(self, text="test frame").grid(row=0, column=0)
         tk.Label(self, text="test frame").grid(row=1, column=0)
