ac488a69080c41a1f65e26083a91efda90f2b5ec
Luis J. Romero <romeroluisj@gmail.com>
2021-11-17 08:21:25 -0500

    Updated LabelFrame class

--- tkinter_code/main/main.py
+++ tkinter_code/main/main.py
@@ -1,3 +1,5 @@
+import tkinter
+
 from tkinter_code.tk.window import Window
 from tkinter_code.tk.frame import Frame
 from tkinter_code.tk.labelframe import LabelFrame
@@ -8,17 +10,17 @@ from tkinter_code.tk.button import Button
 def main():
     main_window = Window("Main Window")
 
-    frame_00 = LabelFrame(main_window, 0, 0)
-    Label(frame_00, 0, 0)
-    Label(frame_00, 1, 1)
+    labelframe_00 = LabelFrame(main_window, 0, 0)
+    Label(labelframe_00, 0, 0)
+    Label(labelframe_00, 1, 1)
 
-    labelframe_11 = LabelFrame(main_window, 1, 1, "")
+    labelframe_11 = LabelFrame(main_window, 1, 1)
     Label(labelframe_11, 0, 0)
     Label(labelframe_11, 1, 1)
 
-    labelframe_22 = Frame(main_window, 2, 2)
-    Button(labelframe_22, 0, 0)
-    Button(labelframe_22, 1, 1)
+    frame_22 = Frame(main_window, 2, 2)
+    Button(frame_22, 0, 0)
+    Button(frame_22, 1, 1)
 
     main_window.start()
 
--- tkinter_code/tk/labelframe.py
+++ tkinter_code/tk/labelframe.py
@@ -3,8 +3,16 @@ import tkinter as tk
 
 class LabelFrame(tk.LabelFrame):
 
-    def __init__(self, window, row=1, column=1, text="lf", relief=tk.GROOVE, bd="2"):
+    def __init__(self, window, row=1, column=1, text=None, relief=None, bd=None):
         super().__init__(window, text=text, relief=relief, bd=bd)
         if text == "lf":
             self.config(text=f"{text}_{row}{column}")
         self.grid(row=row, column=column)
+
+
+"""
+Ref:
+https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/labelframe.html
+relief: default = tk.GROOVE
+bd: default = 2
+"""
