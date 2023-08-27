36f7be32d1a182d130904fd9dab68bc648a3a1e8
Luis J. Romero <romeroluisj@gmail.com>
2023-08-27 17:14:49 -0400

    Revised main, url button, added new file for web functions

--- tkinter_code/main/main.py
+++ tkinter_code/main/main.py
@@ -5,17 +5,21 @@ from tkinter_code.tk.frame import Frame
 from tkinter_code.tk.labelframe import LabelFrame
 from tkinter_code.tk.label import Label
 from tkinter_code.tk.button import Button
+from tkinter_code.web.functions import open_tabs
 
 def main():
     main_window_title = "Main Window"
     main_window = Window(main_window_title)
 
     print_msg = "yay"
+    url_chatgpt = "https://chat.openai.com"
     # Labelframes can have titles; place them at window grid's row #, column #
     # Place label(s) at labelframe grid's row #, column #
 
     labelframe_00 = LabelFrame(main_window, 0, 0, "URL buttons")
-    Button(labelframe_00, 0, 0, "ChatGPT", lambda: webbrowser.open('https://chat.openai.com'))
+    #Button(labelframe_00, 0, 0, "ChatGPT", lambda: webbrowser.open(url_chatgpt))
+    Button(labelframe_00, 0, 0, "ChatGPT", command=open_tabs)
+
 
     # Frames cannot have titles
     frame_10 = Frame(main_window, 1, 0, "raised", 1)
---
+++ tkinter_code/web/functions.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+def open_tabs():
+    urls = [
+        'https://chat.openai.com',
+        'https://www.inquirer.com',
+        'https://www.nytimes.com',
+        'https://www.elpais.com',
+        'https://www.elcomercio.pe'
+    ]
+    for url in urls:
+        webbrowser.open_new_tab(url)
