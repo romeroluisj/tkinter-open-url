11ddb3b83fb62f1d05a7c9fb57bcac4cf8e457d0
Luis J. Romero <romeroluisj@gmail.com>
2023-08-28 21:48:43 -0400

    Cleaned up main, moved labels and buttons to window build function

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from tkinter_open_url.tk.window import Window
-from tkinter_open_url.tk.frame import Frame
-from tkinter_open_url.tk.labelframe import LabelFrame
-from tkinter_open_url.tk.label import Label
-from tkinter_open_url.tk.button import Button
-from tkinter_open_url.web.functions import open_tabs
 
 
 def main():
     main_window_title = "Main Window"
     main_window = Window(main_window_title)
-
-    labelframe_00 = LabelFrame(main_window, 0, 0, "URL buttons")
-    Button(labelframe_00, 0, 0, "ChatGPT", command=open_tabs)
-
     main_window.build()
     main_window.start()
 
--- tkinter_open_url/tk/window.py
+++ tkinter_open_url/tk/window.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter_open_url.tk.button import Button
+from tkinter_open_url.tk.labelframe import LabelFrame
+from tkinter_open_url.web.functions import open_tabs
 
 
-# https://www.pythontutorial.net/tkinter/tkinter-object-oriented-frame/
 class Window(tk.Tk):
 
     def __init__(self, title="Window Title", geometry="500x500"):
@@ -14,23 +16,8 @@ class Window(tk.Tk):
 
     def build(self):
         print(self.title())
+        labelframe_00 = LabelFrame(self, 0, 0, "URL buttons")
+        Button(labelframe_00, 0, 0, "ChatGPT", command=open_tabs)
 
     def start(self):
         self.mainloop()
-
-
-"""
-# without inheritance from tk.Tk
-class Window:
-
-    def __init__(self, title="Window Title", geometry="500x500"):
-        self.window = tk.Tk()
-        self.window.title(title)
-        self.window.geometry(geometry)
-        self.window.lift()
-        self.window.attributes("-topmost", True)
-        self.window.after_idle(self.window.attributes, "-topmost", False)
-
-    def start(self):
-        self.window.mainloop()
-"""
