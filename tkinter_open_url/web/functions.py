5cd42bec77951488c8dbfce6f4bc961a6e4a82d7
Luis J. Romero <romeroluisj@gmail.com>
2023-08-30 07:12:09 -0400

    Added weather.com

--- tkinter_open_url/web/functions.py
+++ tkinter_open_url/web/functions.py
@@ -4,6 +4,7 @@ import webbrowser
 def open_tabs(button_name=None):
     urls = [
         'https://chat.openai.com',
+        'https://weather.com',
         'https://www.inquirer.com',
         'https://www.nytimes.com',
         'https://www.elpais.com',
