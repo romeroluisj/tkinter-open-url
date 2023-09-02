162f0924fd252772250c26f96eba190c86cb078c
Luis J. Romero <romeroluisj@gmail.com>
2023-09-02 18:19:22 -0400

    url_list.txt working now. I had to move it, and I think there is a space to left of Default in same .txt file, but at least it's working now. Good news

--- tkinter_open_url/web/web_functions.py
+++ tkinter_open_url/web/web_functions.py
@@ -1,3 +1,5 @@
+import ast
+import os
 import webbrowser
 
 
@@ -5,20 +7,47 @@ def open_tabs(button_name=None):
     urls = get_url_list(button_name)
 
     if button_name is not None:
-        print(button_name)
+        print("open_tabs function, button_name = " + button_name)
 
     for url in urls:
         webbrowser.open_new_tab(url)
 
 
 def get_url_list(button_name=None):
-    url_list = [
-        'https://chat.openai.com',
-        'https://weather.com',
-        'https://billypenn.com',
-        'https://www.inquirer.com',
-        'https://www.nytimes.com',
-        'https://www.elpais.com',
-        'https://www.elcomercio.pe'
-    ]
-    return url_list
+    print("Current Working Directory:", os.getcwd())  # For debugging
+
+    file_list = os.listdir()
+
+    print("This is the current file list: \n", file_list)
+
+    if button_name is None:
+        msg = "get_url_list, button_name is None = empty, no value"
+        print(msg)
+        return None
+
+    filename = 'url_list.txt'
+
+    # read and print contents of urL_list.txt
+    with open(filename, "r") as file:
+        content = file.read()
+        print("This is the url_list.txt content:\n", content)
+
+    try:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            print("try")
+    except FileNotFoundError:
+        print("FileNotFoundError")
+        return f"{filename} not found in current directory"
+
+    for line in lines:
+        line = line.strip()  # remove trailing whitespace, newline
+        key, value_str = line.split(":", 1)
+
+        if key == button_name:
+            # Convert string representation of list to actual list
+            url_list = ast.literal_eval(value_str)
+            print(url_list)
+            return url_list
+        else:
+            print("key not found")
