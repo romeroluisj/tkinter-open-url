1050af75b6086bc8f2b37553be60579f3a0d4ade
Luis J. Romero <romeroluisj@gmail.com>
2023-09-03 16:01:14 -0400

    in get_url_list function, added comments and google.com if no keys found

--- tkinter_open_url/web/web_functions.py
+++ tkinter_open_url/web/web_functions.py
@@ -5,7 +5,6 @@ import webbrowser
 
 def open_tabs(button_name=None):
     urls = get_url_list(button_name)
-
     for url in urls:
         webbrowser.open_new_tab(url)
 
@@ -16,18 +15,20 @@ def get_url_list(button_name=None):
     filename = "tkinter_open_url/web/url_list.txt"
     lines = read_lines(filename)
 
+    # Split every line into "key, value_str"
     for line in lines:
         line = line.strip()  # remove trailing whitespace, newline
         key, value_str = line.split(":", 1)
 
+        # When key = button_name, convert value_str to actual list
         if key == button_name:
-            # Convert string representation of list to actual list
             url_list = ast.literal_eval(value_str)
             return url_list
 
     # If for-loop finishes, and button_name is not found in keys
     print("button_name not found in keys")
-    return None
+    url_list = ['https://www.google.com/']
+    return url_list
 
 
 def read_lines(filename):
