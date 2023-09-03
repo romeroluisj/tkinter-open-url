ce783f8e448103bc5f549832a6ea7ed963c9a0a6
Luis J. Romero <romeroluisj@gmail.com>
2023-09-03 16:55:42 -0400

    Expanding build functions

--- tkinter_open_url/tk/window.py
+++ tkinter_open_url/tk/window.py
@@ -15,14 +15,19 @@ class Window(tk.Tk):
         self.after_idle(self.attributes, "-topmost", False)
 
     def build(self):
-        labelframe_00 = LabelFrame(self, 0, 0, "URL buttons")
-
-        # URL buttons
+        # Default section
+        labelframe_00 = LabelFrame(self, 0, 0, "Default")
         button_00 = Button(labelframe_00, 0, 0, "Default",
                            command=lambda: open_tabs(button_00.cget('text')))
-        button_10 = Button(labelframe_00, 1, 0, "English",
+
+        # Other sections
+        self.build_language_buttons()
+
+    def build_language_buttons(self):
+        labelframe_10 = LabelFrame(self, 1, 0, "Languages")
+        button_10 = Button(labelframe_10, 1, 0, "English",
                            command=lambda: open_tabs(button_10.cget('text')))
-        button_20 = Button(labelframe_00, 2, 0, "Español",
+        button_20 = Button(labelframe_10, 2, 0, "Español",
                            command=lambda: open_tabs(button_20.cget('text')))
 
     def start(self):
